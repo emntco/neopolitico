5f602b674caadc76d0e28642e0a71a0aa5329c8b
Zayadur Khan <zayadur96@gmail.com>
2016-07-11 10:40:44 -0400

    Rename dev to src

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ gulp.task('default', function() {
 });
 
 gulp.task('minify-css', function() {
-  return gulp.src('dev/assets/css/*.css')
+  return gulp.src('src/assets/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/assets/css'));
 });
 
 gulp.task('minify-html', function() {
-  return gulp.src('dev/*.html')
+  return gulp.src('src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'))
 });
